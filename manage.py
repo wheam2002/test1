82c0b095a5c22c6664057bafdfc8785549da7d2d
root <root@KaiP-1.maas>
2021-06-11 06:51:52 +0000

    test2

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    print()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test1.settings')
     try:
         from django.core.management import execute_from_command_line
